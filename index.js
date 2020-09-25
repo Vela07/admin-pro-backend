7655c32cfe1c50c54e2e1d3a77f55844285632e1
avela <tonyvelamtz@hotmail.com>
2020-09-25 13:18:14 -0500

    Fin sección 10 - Auth

--- index.js
+++ index.js
@@ -11,20 +11,25 @@ const app = express();
 // Configurar CORS
 app.use( cors() );
 
+// Lectura y Parseo del Body
+app.use( express.json() );
+
 // Base de Datos
 dbConnection();
 
-//mean_user
-//PfTxd38pUOXMBSMo
 
 // Rutas
-app.get( '/', (req, res) => {
-    res.json({
-        ok: true,
-        msg: 'Hola mundo'
-    })
-});
+app.use( '/api/usuarios', require( './routes/usuarios' ) );
+app.use( '/api/login', require( './routes/auth' ) );
+
 
 app.listen( process.env.PORT,() => {
     console.log('Servidor corriendo en el puerto ' + process.env.PORT );
 });
+
+
+
+
+// USUARIO Y CONTRASEÑA DE MONGODB COMPASS
+//mean_user
+//PfTxd38pUOXMBSMo
